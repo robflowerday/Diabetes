4871fcfe9ac138a60bc36f4002825b7fa9a62b23
robflowerday <rob.flowerday@hotmail.co.uk>
2024-02-04 01:25:15 +0000

    Revert to default pProgram.cs

--- Diabetes/Program.cs
+++ Diabetes/Program.cs
@@ -1,27 +1,10 @@
-﻿// using System;
-// using System.Collections.Generic;
-// using Diabetes.DynamicMetricRecalculationStrategies;
-//
-// using Diabetes.User;
-//
-//
-// namespace Diabetes
-// {
-//     public class Program
-//     {
-//         public static void Main(string[] args)
-//         {
-//             // Create user configuration handler
-//             string userConfigurationFilePath = "/home/rob/TempUserConfigDir/userConfigurationFile.json";
-//             UserConfigurationHandler userConfigurationHandler =
-//                 new UserConfigurationHandler(userConfigurationFilePath: userConfigurationFilePath);
-//             
-//             // Decide metric recalculation strategy
-//             IDynamicallyRecalculateMetricsStrategy initialStrategy = new RecalculateLongActingOnlyStrategy();
-//             
-//             // Instantiate app
-//             DiabetesManagement dm = new DiabetesManagement(userConfigurationHandler: userConfigurationHandler,
-//                 dynamicallyRecalculateMetricsStrategy: initialStrategy);
-//         }
-//     }
-// }
+﻿namespace Diabetes
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
